1d6c15197e8436882942adfb9581c58b2355e731
Renel Lherisson <renellherisson@gmail.com>
2025-04-19 12:25:50 -0400

    RL | files formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import "./ui/global.css";
+import { inter } from './ui/fonts';
+import './ui/global.css';
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
 }
