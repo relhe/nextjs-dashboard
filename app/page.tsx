7b8af6c8411f97b0be8f89c6337ab5e34557e057
Renel Lherisson <renellherisson@gmail.com>
2025-04-19 13:14:45 -0400

    RL | implemented image interactive using tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,13 @@ export default function Page() {
                         height={760}
                         className="hidden md:block"
                     />
+                    <Image
+                        src="/hero-mobile.png"
+                        alt="Screenshots of the dashboard project showing desktop version"
+                        width={560}
+                        height={620}
+                        className="block md:hidden"
+                    />
                 </div>
             </div>
         </main>
